558435fde34e1d32a97cf770bae5aa55e6779a72
zbud68 <mr.davis68@icloud.com>
2018-12-28 22:48:52 -0600

    Looking Good

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numPlayers = 1
+    var numDice = 5
+    var numSides = 6
+    var endScore = 10000
+    var matchScore = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+        
 }
 
